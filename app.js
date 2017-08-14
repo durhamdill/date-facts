8d080c79f8a42d74dbc6d6e6bc5d5bb76e7f3bcd
Katy Dillard <durhamdill@gmail.com>
2017-08-14 16:17:59 -0400

    figure out seconds computation

--- app.js
+++ app.js
@@ -2,16 +2,36 @@ const moment = require('moment');
 
 const chalk = require('chalk');
 
-// let date = moment("YYYY");
+// LINE 1:
 
 let date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 console.log(`It is ${date}.`);
 
+// LINE 2:
+
 let doY = moment().format("DDDo");
 console.log(`It is the ${doY} day of the year.`);
 
-let now = moment().format("LT");
-let then = undefined;
+// LINE 3:
 
-console.log(now);
+let now = moment();
+let then = moment().hour(0).minute(0).second(0);
+let seconds = now.diff(then, 'seconds');
+console.log(`It is ${seconds} seconds into the day.`);
+
+// LINE 4:
+let leapYear = moment().isLeapYear();
+if (leapYear) {
+  console.log("It is a Leap Year!");
+} else {
+  console.log("It is not a Leap Year.");
+}
+
+// LINE 5:
+let dayST = moment().isDST();
+if (dayST) {
+  console.log("It is Daylight Savings Time!");
+} else {
+  console.log("It is not Daylight Savings Time.");
+}
